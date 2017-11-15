953516ec5f2d419336ba386397ded0025b158a9c
Lee ga hyun <bull1004@naver.com>
2017-11-15 14:22:12 +0900

    birthday

--- button1.py
+++ button1.py
@@ -4,8 +4,10 @@ window =Tk()
 
 b1 = Button(window, text="이가현")
 b2 = Button(window, text="60162146")
+b3 = Button(window, text="0927")
 
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
+b3.grid(row=1, column=0)
 
 window.mainloop()
