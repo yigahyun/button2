ccaf1d27635ebb77cb5ca0a1bded782209ef37bb
Lee ga hyun <bull1004@naver.com>
2017-11-15 14:05:20 +0900

    name and student-id

--- button1.py
+++ button1.py
@@ -2,8 +2,8 @@ from tkinter import *
 
 window =Tk()
 
-b1 = Button(window, text="One")
-b2 = Button(window, text="Two")
+b1 = Button(window, text="이가현")
+b2 = Button(window, text="60162146")
 
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
